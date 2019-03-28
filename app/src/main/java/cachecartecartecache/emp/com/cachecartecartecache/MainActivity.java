ad0e13966aedf7f89a0900a19a0a0bef8b7f70b4
B533P-08\533 <pluc113@hotmail.com>
2019-03-28 14:27:03 -0400

    activity and shit

--- app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/MainActivity.java
+++ app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/MainActivity.java
@@ -1,7 +1,9 @@
 package cachecartecartecache.emp.com.cachecartecartecache;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setListener();
+    }
+
+    private void setListener(){
+        findViewById(R.id.btn_play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToNewGame();
+            }
+        });
+        findViewById(R.id.btn_custom).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToCustom();
+            }
+        });
+    }
+
+    private void sendToNewGame(){
+        Intent sendToNewGame = new Intent(this, GameActivity.class);
+        startActivity(sendToNewGame);
+    }
+
+    private void sendToCustom(){
+        Intent sendToCustomTiles = new Intent(this, CustomTilesActivity.class);
+        startActivity(sendToCustomTiles);
     }
 }
