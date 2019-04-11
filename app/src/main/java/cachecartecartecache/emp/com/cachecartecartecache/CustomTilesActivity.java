296f8aac158d8bd1d47c68c2ad5664014c8125d4
B533P-08\533 <pluc113@hotmail.com>
2019-04-11 14:30:17 -0400

    manifest et quelque fonction

--- app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/CustomTilesActivity.java
+++ app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/CustomTilesActivity.java
@@ -1,6 +1,9 @@
 package cachecartecartecache.emp.com.cachecartecartecache;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,5 +29,28 @@ public class CustomTilesActivity extends AppCompatActivity {
     private void sendToMenu(){
         Intent intentSendMenu = new Intent(this, MainActivity.class);
         startActivity(intentSendMenu);
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 }
