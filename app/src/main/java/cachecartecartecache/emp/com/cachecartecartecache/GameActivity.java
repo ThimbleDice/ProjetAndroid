948167472a6881ef0a170ec168a2db42ab57104e
thimbledice <thimbledice>
2019-04-11 14:31:02 -0400

    image download v1

--- app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/GameActivity.java
+++ app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/GameActivity.java
@@ -1,9 +1,18 @@
 package cachecartecartecache.emp.com.cachecartecartecache;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,11 +36,48 @@ public class GameActivity extends AppCompatActivity {
             Toast.makeText(this, "Not Equal", Toast.LENGTH_LONG).show();
         }
         displayCards();
+        downloadImage("https://www.ssbwiki.com/images/1/11/SSBUSmashBall.png");
     }
 
     private void displayCards(){
+        ImageView imageView = findViewById(R.id.image_card0);
+        imageView.setImageBitmap(cards.get(0).getPicture());
         for (int i=0; i<cards.size(); i++) {
-            Toast.makeText(this, cards.get(i).getCardId(), Toast.LENGTH_LONG).show();
+            //Toast.makeText(this, cards.get(i).getCardId(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void downloadImage(String imageUrl){
+        ImageDownloader imageDownloader = new ImageDownloader();
+        try{
+            imageDownloader.execute(imageUrl);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try{
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap imageBitmap = BitmapFactory.decodeStream(inputStream);
+                return imageBitmap;
+            }catch (MalformedURLException e){
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Bitmap bitmap){
+            final ImageView card0 = findViewById(R.id.image_card0);
+            card0.setImageBitmap(bitmap);
         }
     }
 
