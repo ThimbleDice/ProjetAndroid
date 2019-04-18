4b083d1f1e09358386b3fc85d944b7ee23d9362c
Pierre-Luc Bégin <pluc113@hotmail.com>
2019-04-18 14:27:13 -0400

    test pour appareil photo

--- app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/CustomTilesActivity.java
+++ app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/CustomTilesActivity.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class CustomTilesActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,12 @@ public class CustomTilesActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 sendToMenu();
+            }
+        });
+        findViewById(R.id.btn_take_photo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
             }
         });
     }
@@ -52,5 +61,12 @@ public class CustomTilesActivity extends AppCompatActivity {
             // Camera is not available (in use or does not exist)
         }
         return c; // returns null if camera is unavailable
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
