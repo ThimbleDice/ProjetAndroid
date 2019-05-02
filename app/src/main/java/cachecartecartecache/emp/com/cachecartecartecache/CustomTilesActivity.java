8330a8c3ee12c771e3c3ab4198ccf28008dd3b2f
Riogun13 <pluc113@hotmail.com>
2019-05-02 14:31:09 -0400

    camera marche

--- app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/CustomTilesActivity.java
+++ app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/CustomTilesActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Environment;
@@ -12,6 +13,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -29,8 +31,8 @@ public class CustomTilesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_custom_tiles);
         setListener();
 
-        takePictureButton = (Button) findViewById(R.id.btn_take_photo);
-        imageView = (ImageView) findViewById(R.id.imageView_photo);
+        takePictureButton = findViewById(R.id.btn_take_photo);
+        imageView = findViewById(R.id.imageView_photo);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             takePictureButton.setEnabled(false);
@@ -48,14 +50,14 @@ public class CustomTilesActivity extends AppCompatActivity {
         }
     }
 
-    /**@Override
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 100) {
-            if (resultCode == RESULT_OK) {
-                imageView.setImageURI(file);
-            }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
         }
-    }*/
+    }
 
     private void setListener(){
         findViewById(R.id.btn_back_to_menu).setOnClickListener(new View.OnClickListener() {
@@ -71,6 +73,12 @@ public class CustomTilesActivity extends AppCompatActivity {
                 //takePicture();
             }
         });
+        findViewById(R.id.btn_add_custom_tiles).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //add fonction pour ajouter une new tiles
+            }
+        });
     }
 
     private void sendToMenu(){
@@ -84,28 +92,5 @@ public class CustomTilesActivity extends AppCompatActivity {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
-
-   /** public void takePicture(View view) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        file = Uri.fromFile(getOutputMediaFile());
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
-
-        startActivityForResult(intent, 100);
-    }
-
-    private static File getOutputMediaFile(){
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "CameraDemo");
-
-        if (!mediaStorageDir.exists()){
-            if (!mediaStorageDir.mkdirs()){
-                return null;
-            }
-        }
-
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        return new File(mediaStorageDir.getPath() + File.separator +
-                "IMG_"+ timeStamp + ".jpg");
-    }*/
 
 }
