5776952a07efdc5b1ca85b214c44c4a5daccec6d
B533P-07\533 <thimbledice>
2019-05-02 13:24:33 -0400

    Pair programming avec Marco pour connection BD

--- app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/MainActivity.java
+++ app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/MainActivity.java
@@ -1,9 +1,17 @@
 package cachecartecartecache.emp.com.cachecartecartecache;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
