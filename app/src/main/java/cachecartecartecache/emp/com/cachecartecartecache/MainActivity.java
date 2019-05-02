8758880529b6bcd6056d724f051d9b3f944021a7
B533P-07\533 <thimbledice>
2019-05-02 14:34:21 -0400

    Upload/Download

--- app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/MainActivity.java
+++ app/src/main/java/cachecartecartecache/emp/com/cachecartecartecache/MainActivity.java
@@ -1,25 +1,49 @@
 package cachecartecartecache.emp.com.cachecartecartecache;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setListener();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        uploadImage();
+        downloadImage("ImagePrincipale/250px-Zelda_SSBU.png");
     }
 
     private void setListener(){
@@ -45,5 +69,67 @@ public class MainActivity extends AppCompatActivity {
     private void sendToCustom(){
         Intent sendToCustomTiles = new Intent(this, CustomTilesActivity.class);
         startActivity(sendToCustomTiles);
+    }
+
+    private void uploadImage(){
+        // Get the data from an ImageView as bytes
+        ImageView imageView = findViewById(R.id.imageView_smashBall);
+        imageView.setDrawingCacheEnabled(true);
+        imageView.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+        String md5String = md5(simpleDateFormat.format(new Date()));
+        final StorageReference thatRef = mStorageRef.child("ImagePrincipale/"+md5String+".png");
+        UploadTask uploadTask = thatRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Toast.makeText(MainActivity.this, "Failed to upload, please try again later", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(MainActivity.this, "onSuccess", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void downloadImage(String fileName){
+        mStorageRef.child(fileName).getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                // Use the bytes to display the image
+                ImageView imageView = findViewById(R.id.imageView_smashBall);
+                imageView.setImageBitmap(BitmapFactory.decodeByteArray(bytes, 0, bytes.length));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Toast.makeText(MainActivity.this, "Failed to download, please try again later", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public String md5(String s) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            for (int i=0; i<messageDigest.length; i++)
+                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
